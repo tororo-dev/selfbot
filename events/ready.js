38a49e108b30a82ccabb319e4a835322954165e7
tororo-dev <tororodev@gmail.com>
2024-09-20 08:22:17 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,52 +7,23 @@ module.exports = {
     async execute(client) {
         const getExtendURL = await RichPresence.getExternal(
             client,
-            '1259032519542837349',
-            'https://cdn.discordapp.com/app-assets/1259032519542837349/1262247475960610867.png',
+            'bot_id',
+            '',
         );
-        const status = new RichPresence(client)
-        .setApplicationId('1259032519542837349')
+        const richPresence = new RichPresence(client)
+        .setApplicationId('bot_id')
         .setType('COMPETING')
-        .setName(client.guilds.cache.size + 'サーバー')
-        .setDetails('ぴあこーど')
+        .setName('Hello world!')
+        .setDetails('しもべ | *help')
         .setStartTimestamp(Date.now())
         .setAssetsLargeImage(getExtendURL[0].external_asset_path)
-        .setAssetsLargeText('ぴのぴのもあそばない！')
-        .setAssetsSmallImage('1262252019335434362')
-        .setAssetsSmallText('ﾋﾟ')
-        .addButton('めいん', 'https://discord.com/users/1205349856219103262')
-        .addButton("ふぁんくらぶ┃EWPM", 'https://discord.gg/ewpm');
+        .setAssetsSmallImage('')
+        .addButton('Developed by torocord', 'https://discord.com/users/1205349856219103262');
+
+        const status = new CustomStatus(client).setEmoji('✅').setState(client.guilds.cache.size + ' servers');
         
         console.log(client.user.tag + " is online!");
-        
-        client.user.setPresence({ activities: [status] });
-
-        const channel = await client.channels.fetch(process.env.channel)
-      
-        async function bump() {
-            await channel.sendSlash('302050872383242240', 'bump')
-            console.log('bump!')
-        }
-      
-        async function dissoku_up() {
-            await channel.sendSlash('761562078095867916', 'dissoku up')
-            console.log('dissoku up!')
-        }
-      
-        async function up() {
-            await channel.sendSlash('903541413298450462', 'up')
-            console.log('up!')
-        }
-      
-        function loop() {
-            setTimeout(function () {
-                bump();
-                dissoku_up();
-                up();
-            }, 3610000);
-        }
-
-        loop();
+        client.user.setPresence({ activities: [richPresence, status] });
     },
 };
  
