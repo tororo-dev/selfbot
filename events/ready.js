103b1591fbb6596a2146c2af636cac702e94ae42
tororo-dev <tororodev@gmail.com>
2024-09-20 19:01:13 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,17 +7,17 @@ module.exports = {
     async execute(client) {
         const getExtendURL = await RichPresence.getExternal(
             client,
-            'bot_id',
-            '',
+            '1264835937767129181',
+            'https://cdn.discordapp.com/app-assets/1264835937767129181/1286625549615239201.png',
         );
         const richPresence = new RichPresence(client)
-        .setApplicationId('bot_id')
-        .setType('COMPETING')
+        .setApplicationId('1264835937767129181')
+        .setType('LISTENING')
         .setName('Hello world!')
         .setDetails('しもべ | *help')
         .setStartTimestamp(Date.now())
         .setAssetsLargeImage(getExtendURL[0].external_asset_path)
-        .setAssetsSmallImage('')
+        .setAssetsSmallImage('1264839191532142656')
         .addButton('Developed by torocord', 'https://discord.com/users/1205349856219103262');
 
         const status = new CustomStatus(client).setEmoji('✅').setState(client.guilds.cache.size + ' servers');
